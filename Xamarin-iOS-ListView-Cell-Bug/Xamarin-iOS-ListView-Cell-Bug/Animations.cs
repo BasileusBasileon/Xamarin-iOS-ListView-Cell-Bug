bba15f1f61df5ea1bfd3b1e0cac96d2a70342441
Nils Dunkel <InimicusD@googlemail.com>
2020-02-11 11:36:19 +0100

    Add project files.

---
+++ Xamarin-iOS-ListView-Cell-Bug/Xamarin-iOS-ListView-Cell-Bug/Animations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_iOS_ListView_Cell_Bug
+{
+	public static class Animations
+	{
+		public static Task<bool> LayoutBoundsTo(this VisualElement element, Rectangle posFrom, Rectangle posTo, uint length = 250, Easing easing = null)
+		{
+			Rectangle transform(double t)
+				=> new Rectangle(
+					posFrom.X + t * (posTo.X - posFrom.X),
+					posFrom.Y + t * (posTo.Y - posFrom.Y),
+					posFrom.Width + t * (posTo.Width - posFrom.Width),
+					posFrom.Height + t * (posTo.Height - posFrom.Height)
+				);
+
+			void callback(Rectangle rect)
+				=> AbsoluteLayout.SetLayoutBounds(element, rect);
+
+			return DoAnimation(element, nameof(LayoutBoundsTo), transform, callback, length, easing);
+		}
+
+		private static Task<bool> DoAnimation<T>(VisualElement element, string name, Func<double, T> transform, Action<T> callback, uint length, Easing easing)
+		{
+			TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+
+			element.Animate(name, transform, callback, 16, length, easing ?? Easing.Linear, (v, r) => tcs.TrySetResult(r));
+			return tcs.Task;
+		}
+	}
+}
---
+++ Xamarin-iOS-ListView-Cell-Bug/Xamarin-iOS-ListView-Cell-Bug/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_iOS_ListView_Cell_Bug
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+			AbsoluteLayout.SizeChanged += AbsoluteLayout_SizeChanged;
+		}
+
+		private Rectangle _animationDestinationLeft, _animationStartLeft, _animationDestinationRight, _animationStartRight;
+		private Stack<View> _viewStack = new Stack<View>();
+
+		private void AbsoluteLayout_SizeChanged(object sender, EventArgs e)
+		{
+			// On Portrait we want to the view to be 75% of the width, on landscape 50%
+			double newWidth = (AbsoluteLayout.Width > AbsoluteLayout.Height) ? 0.5 : 0.75;
+
+			// Setting the rectangles used for the animation
+			_animationStartLeft = new Rectangle(-2d * newWidth, 0d, newWidth, 1d);
+			_animationDestinationLeft = new Rectangle(0d, 0d, newWidth, 1d);
+			_animationStartRight = new Rectangle(1d + 2d * newWidth, 0d, newWidth, 1d);
+			_animationDestinationRight = new Rectangle(1d, 0d, newWidth, 1d);
+
+			// Changing the size of the animated in views
+			foreach (View view in _viewStack)
+			{
+				AbsoluteLayout.SetLayoutBounds(view, _animationDestinationRight);
+			}
+		}
+
+		readonly List<string> items = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n" };
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			ListView listView = new ListView() { BackgroundColor = Color.Blue };
+			listView.ItemTemplate = new DataTemplate(() =>
+			{
+				ViewCell viewCell = new ViewCell();
+				BoxView box = new BoxView() { Color = Color.Black };
+				viewCell.View = box;
+				return viewCell;
+			});
+			listView.ItemsSource = items;
+
+			_viewStack.Push(listView);
+			AbsoluteLayout.Children.Add(listView, new Rectangle(1d, 0d, 0d, 1d), AbsoluteLayoutFlags.All);
+			_ = listView.LayoutBoundsTo(_animationStartRight, _animationDestinationRight, 200, Easing.CubicInOut);
+		}
+	}
+}
